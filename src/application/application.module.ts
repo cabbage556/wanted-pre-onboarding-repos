fa55bdcee9ae50b1a9a38a31934f737bb7b217e7
cabbage556 <liberty556@gmail.com>
2023-10-07 01:20:03 +0900

    Refactor: "변경 PrismaModule"
    - PrismaModule을 글로벌 모듈로 변경

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ApplicationController } from './application.controller';
 import { ApplicationService } from './application.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [ApplicationController],
   providers: [ApplicationService],
 })
--- src/job-posting/job-posting.module.ts
+++ src/job-posting/job-posting.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { JobPostingController } from './job-posting.controller';
 import { JobPostingService } from './job-posting.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [JobPostingController],
   providers: [JobPostingService],
 })
