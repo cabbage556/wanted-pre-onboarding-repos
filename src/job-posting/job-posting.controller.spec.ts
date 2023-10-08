e5eb20f3ef4fa53ca8ba109f7b6adc4c768d5a3e
cabbage556 <liberty556@gmail.com>
2023-10-08 22:06:28 +0900

    TEST: "추가 JobPostingController 유닛 테스트 파일"
    - JobPostingController 유닛 테스트 파일을 추가하였음
    - 유닛 테스트를 진행하기 위한 설정을 추가하였음

--- src/job-posting/job-posting.controller.spec.ts
+++ src/job-posting/job-posting.controller.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JobPostingController } from './job-posting.controller';
+import { JobPostingService } from './job-posting.service';
 
 describe('JobPostingController', () => {
-  let controller: JobPostingController;
+  let jobPostingController: JobPostingController;
+  let jobPostingService: JobPostingService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [JobPostingController],
+      providers: [
+        {
+          provide: JobPostingService,
+          useValue: {
+            createJobPosting: jest.fn(),
+            getJobPostings: jest.fn(),
+            searchJobPostings: jest.fn(),
+            getDetailPage: jest.fn(),
+            updateJobPosting: jest.fn(),
+            deleteJobPosting: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<JobPostingController>(JobPostingController);
+    jobPostingController =
+      module.get<JobPostingController>(JobPostingController);
+    jobPostingService = module.get<JobPostingService>(JobPostingService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('controller should be defined', () => {
+    expect(jobPostingController).toBeDefined();
+  });
+
+  it('service should be defined', () => {
+    expect(jobPostingService).toBeDefined();
   });
 });
