7d72cf7316a1607791e4adbd098f87dca7378712
cabbage556 <liberty556@gmail.com>
2023-10-08 23:54:37 +0900

    TEST: "추가 JobPostingController createJobPosting 유닛 테스트"

--- src/job-posting/job-posting.controller.spec.ts
+++ src/job-posting/job-posting.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JobPostingController } from './job-posting.controller';
 import { JobPostingService } from './job-posting.service';
+import { CreateJobPostingDto } from './dto';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('JobPostingController', () => {
   let jobPostingController: JobPostingController;
@@ -15,8 +17,11 @@ describe('JobPostingController', () => {
           useValue: {
             createJobPosting: jest.fn(),
             getJobPostings: jest.fn(),
+            searchInCompany: jest.fn(),
+            searchInPostion: jest.fn(),
             searchJobPostings: jest.fn(),
             getDetailPage: jest.fn(),
+            getJobPostingById: jest.fn(),
             updateJobPosting: jest.fn(),
             deleteJobPosting: jest.fn(),
           },
@@ -35,5 +40,49 @@ describe('JobPostingController', () => {
 
   it('service should be defined', () => {
     expect(jobPostingService).toBeDefined();
+  });
+
+  describe('createJobPosting', () => {
+    const dto: CreateJobPostingDto = {
+      content: '원티드랩에서 NestJS 백엔드 개발자를 채용 중입니다......',
+      position: 'NestJS 백엔드 개발자',
+      stack: '#NestJS #Node.js',
+      rewards: 500000,
+      companyId: 1,
+    };
+
+    it('생성한 채용공고 레코드를 리턴해야 함', () => {
+      jest
+        .spyOn(jobPostingService, 'createJobPosting')
+        .mockImplementationOnce((dto) =>
+          Promise.resolve({
+            id: 1,
+            createdAt: new Date(2023, 9, 8, 23, 5, 30, 333),
+            updatedAt: new Date(2023, 9, 8, 23, 5, 30, 333),
+            ...dto,
+            stack: dto.stack,
+            rewards: dto.rewards,
+          }),
+        );
+
+      expect(
+        jobPostingController.createJobPosting(dto), //
+      ).resolves.toEqual({
+        id: 1,
+        createdAt: new Date(2023, 9, 8, 23, 5, 30, 333),
+        updatedAt: new Date(2023, 9, 8, 23, 5, 30, 333),
+        ...dto,
+      });
+    });
+
+    it('DB 연결에 문제가 있으면 InternalServerErrorException 예외를 던져야 함', () => {
+      jest
+        .spyOn(jobPostingService, 'createJobPosting')
+        .mockRejectedValueOnce(new InternalServerErrorException());
+
+      expect(
+        jobPostingController.createJobPosting(dto), //
+      ).rejects.toThrowError(new InternalServerErrorException());
+    });
   });
 });
