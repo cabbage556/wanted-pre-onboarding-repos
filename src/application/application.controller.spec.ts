ef3f0a84d96f328a563118bdc339841e18b06ec3
cabbage556 <liberty556@gmail.com>
2023-10-09 15:06:32 +0900

    TEST: "추가 ApplicationController 유닛 테스트"
    - ApplicationController 유닛 테스트 파일 추가
    - ApplicationController 유닛 테스트 초기 설정

--- src/application/application.controller.spec.ts
+++ src/application/application.controller.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApplicationController } from './application.controller';
+import { ApplicationService } from './application.service';
 
 describe('ApplicationController', () => {
   let controller: ApplicationController;
+  let service: ApplicationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ApplicationController],
+      providers: [
+        {
+          provide: ApplicationService,
+          useValue: {
+            createApplication: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ApplicationController>(ApplicationController);
+    service = module.get<ApplicationService>(ApplicationService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
   });
 });
