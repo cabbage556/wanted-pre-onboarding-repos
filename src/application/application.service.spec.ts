96501b1cf3ac8a8788ca0e5be00b4378d2e45574
cabbage556 <liberty556@gmail.com>
2023-10-09 14:35:27 +0900

    TEST: "추가 ApplicationService 유닛 테스트"
    - ApplicationService 유닛 테스트 파일 추가
    - ApplicationService 유닛 테스트 초기 설정

--- src/application/application.service.spec.ts
+++ src/application/application.service.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApplicationService } from './application.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('ApplicationService', () => {
   let service: ApplicationService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ApplicationService],
+      providers: [
+        ApplicationService,
+        {
+          provide: PrismaService,
+          useValue: {
+            application: {
+              create: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ApplicationService>(ApplicationService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(prismaService).toBeDefined();
   });
 });
