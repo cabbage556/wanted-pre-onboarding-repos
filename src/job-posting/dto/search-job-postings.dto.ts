0fb272bb26323459930771336ce264522af15b1a
cabbage556 <liberty556@gmail.com>
2023-10-09 23:03:52 +0900

    Refactor: "추가 SearchJobPostingsDto search 유효성 검사"
    - 1글자부터 100글자까지만 입력 가능하도록 유효성 검사 추가

--- src/job-posting/dto/search-job-postings.dto.ts
+++ src/job-posting/dto/search-job-postings.dto.ts
@@ -1,8 +1,9 @@
-import { IsNotEmpty, IsString, IsIn } from 'class-validator';
+import { IsNotEmpty, IsString, IsIn, Length } from 'class-validator';
 
 export class SearchJobPostingsDto {
   @IsNotEmpty()
   @IsString()
+  @Length(1, 100) // 1글자부터 position 필드 최대 길이 100글자까지
   search: string;
 
   @IsIn(['position', 'company'])
