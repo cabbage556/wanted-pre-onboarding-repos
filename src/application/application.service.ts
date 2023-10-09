42ddfd82d314786e810d78d914bf6362bdb5d973
cabbage556 <liberty556@gmail.com>
2023-10-09 14:31:49 +0900

    Fix: "변경 PrismaService import"
    - PrismaService를 임포트하지 못하는 에러 해결

--- src/application/application.service.ts
+++ src/application/application.service.ts
@@ -1,5 +1,5 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateApplicationDto } from './dto';
 import { Application } from '@prisma/client';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
