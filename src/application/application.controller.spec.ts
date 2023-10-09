134cf8290ea9b5f32844fdf7fbb44458826d89af
cabbage556 <liberty556@gmail.com>
2023-10-09 15:32:57 +0900

    TEST: "추가 ApplicationController createApplication 유닛 테스트"

--- src/application/application.controller.spec.ts
+++ src/application/application.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApplicationController } from './application.controller';
 import { ApplicationService } from './application.service';
+import { ForbiddenException } from '@nestjs/common';
 
 describe('ApplicationController', () => {
   let controller: ApplicationController;
@@ -29,5 +30,41 @@ describe('ApplicationController', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createApplication', () => {
+    const dto = {
+      userId: 1,
+      jobPostingId: 1,
+    };
+
+    it('지원내역을 리턴해야 함', () => {
+      jest //
+        .spyOn(service, 'createApplication')
+        .mockResolvedValueOnce({
+          id: 1,
+          createdAt: new Date(2023, 9, 9, 6, 10, 37, 112),
+          ...dto,
+        });
+
+      expect(
+        controller.createApplication(dto), //
+      ).resolves.toEqual({
+        id: 1,
+        createdAt: new Date(2023, 9, 9, 6, 10, 37, 112),
+        userId: 1,
+        jobPostingId: 1,
+      });
+    });
+
+    it('ForbiddenException 예외를 던져야 함', () => {
+      jest
+        .spyOn(service, 'createApplication')
+        .mockRejectedValueOnce(new ForbiddenException('이미 지원하였음'));
+
+      expect(
+        controller.createApplication(dto), //
+      ).rejects.toThrowError(new ForbiddenException('이미 지원하였음'));
+    });
   });
 });
